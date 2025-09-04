542563fba0f7f6004f0a98f0b28c3d68337f054d
SoraOrihara <caca66z@gmail.com>
2025-09-04 09:52:00 -0300

    Entidades mapeadas no banco de dados

--- src/main/java/br/com/springEstudo/AutorLivro/infraestructure/entities/AutorEntity.java
+++ src/main/java/br/com/springEstudo/AutorLivro/infraestructure/entities/AutorEntity.java
@@ -1,5 +1,75 @@
 package br.com.springEstudo.AutorLivro.infraestructure.entities;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_autor")
 public class AutorEntity {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.UUID)
+	private UUID id;
+	
+	@Column(unique = true,nullable = false)
+	private String nome;
+	
+	private LocalDate dataNascimento;
+	
+	@OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<LivroEntity> livros = new HashSet<>();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Set<LivroEntity> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(Set<LivroEntity> livros) {
+		this.livros = livros;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public AutorEntity() {
+		super();
+	}
+
+	public AutorEntity(String nome, LocalDate dataNascimento, Set<LivroEntity> livros) {
+		super();
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.livros = livros;
+	}
+	
+	
+	
+	
 }
--- src/main/java/br/com/springEstudo/AutorLivro/infraestructure/entities/LivroEntity.java
+++ src/main/java/br/com/springEstudo/AutorLivro/infraestructure/entities/LivroEntity.java
@@ -1,5 +1,85 @@
 package br.com.springEstudo.AutorLivro.infraestructure.entities;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_livro")
 public class LivroEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(unique = true, nullable = false)
+	private String titulo;
+
+	private String anoPublicacao;
+
+	@Column(nullable = false)
+	private String genero;
+
+	@ManyToOne
+	@JoinColumn(name = "autor_Id")
+	private AutorEntity autor;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAnoPublicacao() {
+		return anoPublicacao;
+	}
+
+	public void setAnoPublicacao(String anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public AutorEntity getAutor() {
+		return autor;
+	}
+
+	public void setAutor(AutorEntity autor) {
+		this.autor = autor;
+	}
+
+	public LivroEntity() {
+		super();
+	}
+
+	public LivroEntity(String titulo, String anoPublicacao, String genero, AutorEntity autor) {
+		super();
+		this.titulo = titulo;
+		this.anoPublicacao = anoPublicacao;
+		this.genero = genero;
+		this.autor = autor;
+	}
+
+	
+	
+	
 }
